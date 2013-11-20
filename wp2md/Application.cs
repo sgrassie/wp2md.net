e0d88642731ed8dd738de84fa930a19678ae0f0b
Stuart Grassie <sgrassie@gmail.com>
2013-11-20 22:34:39 +0000

    Added new class to hold actual application.

---
+++ wp2md/Application.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wp2md
+{
+    class Application
+    {
+        private Options _options;
+        private Action<string> _logger;
+
+        public Application(Action<string> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Execute(Options options)
+        {
+            _options = options;
+        }
+    }
+}
